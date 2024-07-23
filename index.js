4d1d3bb066f1372e0288eb38800d2bf8ee29b9f8
Yufan Yang <yyf91699@gmail.com>
2024-07-22 22:40:29 -0600

    build a server

--- index.js
+++ index.js
@@ -15,3 +15,37 @@ evnetEmitter.on('lunch',() => {
 
 evnetEmitter.emit('lunch');
 evnetEmitter.emit('lunch');
+
+const{readFileSync} = require("fs");
+const txt = readFileSync('./hello.txt');
+console.log(txt);
+console.log("Do this");
+
+// readFile('./hello.txt','utf8', (err, txt) => {
+//     console.log(txt);
+// });
+
+console.log("2nd");
+
+const {readFile} = require('fs').promises;
+async function hello() {
+const file = await readFile('./hello.txt', 'utf8');
+console.log(file);
+}
+
+const myModule = require('./my-module');
+console.log(myModule)
+
+const express = require('express')
+
+const app = express();
+
+app.get('/', async (request, response) => {
+        // if (err){
+        //     response.status(500).send('sorry, out of order')
+        // }
+        response.send( await readFile('./hello.txt','utf8'));
+
+});
+
+app.listen(process.env.PORT || 3000, () => console.log("app is available on local host port http://localhost:3000"))
---
+++ my-module.js
@@ -0,0 +1,4 @@
+module.exports = {
+    science: 'science'
+}
+
